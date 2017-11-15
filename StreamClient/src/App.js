19431b8e8bb3f1b3437a8f4bc61b022ea6821299
tareef88 <tareefha@gmail.com>
2017-11-15 19:03:04 +0100

    i made my first branch to edit

--- StreamClient/src/App.js
+++ StreamClient/src/App.js
@@ -7,7 +7,7 @@ const Dimensions = require('Dimensions');
 
 const {height,width} = Dimensions.get('window');
 
-
+// i made my first branch to edit 
 export default class App extends React.Component {
 
   createClient() {
